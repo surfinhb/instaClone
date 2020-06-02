a7ddd486cd0c77cbd76e995f34c11f5369aefa1f
Jake Shapiro <jakeshapiro@jakes-mbp-2.lan>
2020-06-01 18:02:37 -0700

    instaClone project started

---
+++ instaClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  instaClone
+//
+//  Created by Jake Shapiro on 6/1/20.
+//  Copyright © 2020 Jake Shapiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
